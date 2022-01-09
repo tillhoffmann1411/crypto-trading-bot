df1a36bc1be17ba2b1dc45ed2873c622c726aa96
Till Hoffmann <tillhoffmann1411@gmail.com>
2022-01-09 15:35:00 +0100

    added config file and optimized algorithm

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
-import websocket, json, numpy as np, talib, pprint, os
+import websocket, json, numpy as np, talib, pprint, os, yaml
 from binance.enums import *
 from binance.client import Client
 from dotenv import load_dotenv
 
-load_dotenv()  # take environment variables from .env.
+load_dotenv()  # take environment variables from .env
+
+with open("config.yml", "r") as ymlfile:
+    cfg = yaml.load(ymlfile, yaml.Loader)
 
 API_KEY = os.environ.get('BINANCE_KEY')
 API_SECRET = os.environ.get('BINACNE_SECRET')
-TEST_API_KEY = os.environ.get('BINANCE_KEY_TEST')
-TEST_API_SECRET = os.environ.get('BINACNE_SECRET_TEST')
 
-RSI_PERIOD = 14
-OVERSOLD_THRESHOLD = 30
-OVERBOUGHT_THRESHOLD = 70
-TRADE_SYMBOL = 'ETHEUR'
-TRADE_QUNATITY = 0.0072
-SOCKET = 'wss://stream.binance.com:9443/ws/etheur@kline_1m'
+RSI_PERIOD = int(cfg['RSI_PERIOD'])
+OVERSOLD_THRESHOLD = int(cfg['OVERSOLD_THRESHOLD'])
+OVERBOUGHT_THRESHOLD = int(cfg['OVERBOUGHT_THRESHOLD'])
+TRADE_SYMBOL = cfg['TRADE_SYMBOL']
+TRADE_QUNATITY = float(cfg['TRADE_QUNATITY'])
+CANDLE_INTERVAL = cfg['CANDLE_INTERVAL']
+SOCKET = 'wss://stream.binance.com:9443/ws/{}@kline_{}'.format(TRADE_SYMBOL.lower(), CANDLE_INTERVAL)
+COIN = TRADE_SYMBOL[0:3]
+STABLE = TRADE_SYMBOL[-3:]
 
 closes = []
 in_position = False
-client = Client(TEST_API_KEY, TEST_API_SECRET, testnet=True)
+client = Client(API_KEY, API_SECRET)
 
-def order(quantity, symbol, side, order_type=ORDER_TYPE_MARKET, test=True):
+def order(quantity, symbol, side, order_type=ORDER_TYPE_MARKET):
     try:
-        order = None
-        if test:
-            order = client.create_test_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
-        else:
-            order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
-        print(order)
+        order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
+        print('order')
     except Exception as e:
         print('Error!')
         print(e)
@@ -37,7 +37,13 @@ def order(quantity, symbol, side, order_type=ORDER_TYPE_MARKET, test=True):
         
         
 def on_open(ws):
-    print('opened connection')
+    print('opened connection...')
+    print('Candle Interval: {}'.format(CANDLE_INTERVAL))
+    print('RSI Period: {}'.format(RSI_PERIOD))
+    print('Oversold by: {}'.format(OVERSOLD_THRESHOLD))
+    print('Overbought by: {}'.format(OVERBOUGHT_THRESHOLD))
+    print('Traded symbol: {}'.format(TRADE_SYMBOL))
+    print('Traded quantity: {}'.format(TRADE_QUNATITY))
     
 def on_message(ws, message):
     global closes, in_position
@@ -49,23 +55,26 @@ def on_message(ws, message):
     close = candle['c']
 
     if is_candle_closed:
-        closes.append(float(close))
         
         if len(closes) > RSI_PERIOD:
             np_closes = np.array(closes)
             rsi = talib.RSI(np_closes, RSI_PERIOD)
-            print(rsi)
             last_rsi = rsi[-1]
             print(last_rsi)
+            closes.append(float(close))
             
-            if last_rsi > OVERBOUGHT_THRESHOLD and in_position:
-                print('Sell!!!')
+            eth_balance = client.get_asset_balance(asset=COIN)['free']
+            eur_balance = client.get_asset_balance(asset=STABLE)['free']
+            price_for_one_trade = round(float(close) * float(TRADE_QUNATITY), 3)
+            
+            if last_rsi > OVERBOUGHT_THRESHOLD and eth_balance >= TRADE_QUNATITY and in_position:
+                print("Sell {} {} for: {}".format(TRADE_QUNATITY, COIN, price_for_one_trade))
                 succeeded = order(quantity=TRADE_QUNATITY, symbol=TRADE_SYMBOL, side=SIDE_SELL)
                 if succeeded:
                     in_position = False
             
-            if last_rsi < OVERSOLD_THRESHOLD and not in_position:
-                print('Buy!!!')
+            if last_rsi < OVERSOLD_THRESHOLD and eur_balance >= price_for_one_trade and not in_position:
+                print("Buy {} {} for: {} €".format(TRADE_QUNATITY, COIN, price_for_one_trade))
                 succeeded = order(quantity=TRADE_QUNATITY, symbol=TRADE_SYMBOL, side=SIDE_BUY)
                 if succeeded:
                     in_position = True
@@ -78,7 +87,7 @@ def on_error(ws, error):
     print('error: ' + str(error))
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":    
     ws = websocket.WebSocketApp(SOCKET,
                               on_open=on_open,
                               on_message=on_message,
